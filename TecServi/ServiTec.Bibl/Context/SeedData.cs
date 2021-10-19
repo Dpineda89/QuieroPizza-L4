995ecc0b00b148aec1f8f9953f6620cae64546d8
Darwin Pineda <darwin_pineda@unah.hn>
2021-10-19 02:37:19 -0600

    Subiendo_MAntenimiento de productos Servitec

---
+++ TecServi/ServiTec.Bibl/Context/SeedData.cs
@@ -0,0 +1,52 @@
+﻿using ServiTec.Database.Context;
+using ServiTec.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiTec.Bibl.Context
+{
+    public class SeedData : DropCreateDatabaseIfModelChanges<ServiTexContext>
+    {
+        protected override void Seed(ServiTexContext context)
+        {
+            var categoria = context.Categorias.Add(new Categoria
+            {
+                Descripcion = "Electronico",
+            });
+
+            context.Productos.Add(new Producto
+            {
+                Descripcion = " Monitor Curvo 24.0 ",
+                Precio = 2600.00,
+                IdCategoria = categoria.Id,
+            });
+
+            context.Productos.Add(new Producto
+            {
+                Descripcion = " Cable UTP Cat-6 caja 232 Pies ",
+                Precio = 1700.00,
+                IdCategoria = categoria.Id,
+            });
+
+            context.Productos.Add(new Producto
+            {
+                Descripcion = " Case Army-Z20 Negro 32cm ",
+                Precio = 1100.00,
+                IdCategoria = categoria.Id,
+            });
+
+            context.Productos.Add(new Producto
+            {
+                Descripcion = " Silla Gamer Full Relax ",
+                Precio = 5400.00,
+                IdCategoria = categoria.Id,
+            });
+            context.SaveChanges();
+            base.Seed(context);
+        }
+    }
+}
---
+++ TecServi/ServiTec.webAdmin/Controllers/CategoriasController.cs
@@ -0,0 +1,136 @@
+﻿using ServiTec.Bibl;
+using ServiTec.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiTec.webAdmin.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        CategoriaBibl _categoriasBibl;
+        public CategoriasController()
+        {
+            _categoriasBibl = new CategoriaBibl();
+        }
+
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            var listadeCategorias = _categoriasBibl.ObtenerCategorias();
+
+            return View(listadeCategorias);
+        }
+
+        public ActionResult Crear()
+        {
+            var nuevoCategoria = new Categoria();
+            return View(nuevoCategoria);
+        }
+
+
+        [HttpPost]
+        public ActionResult Crear(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                if (categoria.Descripcion != categoria.Descripcion.Trim())
+                {
+                    ModelState.AddModelError("Descripcion", "La Descripción no debe contener espacios al inicio o al final");
+
+                    return View(categoria);
+                }
+
+                var result = this._categoriasBibl.CrearCategoria(categoria);
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+
+        [HttpPost]
+        public ActionResult Editar(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                if (categoria.Descripcion != categoria.Descripcion.Trim())
+                {
+                    ModelState.AddModelError("Descripcion", "La Descripción no debe contener espacios al inicio o al final");
+
+                    return View(categoria);
+                }
+
+                var result = this._categoriasBibl.EditarCategoria(categoria);
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public ActionResult Eliminar(Categoria categoria)
+        {
+            var result = this._categoriasBibl.EliminarCategoria(categoria);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var categoria = this._categoriasBibl.ObtenerCategoria(id.Value);
+
+            if (categoria == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return this.View(categoria);
+        }
+
+        [HttpGet]
+        public ActionResult Detalle(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var categoria = this._categoriasBibl.ObtenerCategoria(id.Value);
+
+            if (categoria == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return this.View(categoria);
+        }
+
+
+        [HttpGet]
+        public ActionResult Eliminar(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var categoria = this._categoriasBibl.ObtenerCategoria(id.Value);
+
+            if (categoria == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return this.View(categoria);
+        }
+    }
+}
