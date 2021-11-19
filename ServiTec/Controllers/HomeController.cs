a1ba4d0088118efa684ccf6cb52587d6ba7cd672
Joel Ramiro Caballero Castellanos <joel.ramirocc@outlook.com>
2021-11-19 17:11:19 -0600

    avance 2

--- ServiTec/Controllers/HomeController.cs
+++ ServiTec/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ServiTec.Bibl;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +13,10 @@ namespace ServiTec.web.Controllers
         //GET:Home
         public ActionResult Index()
         {
-            return View();
+            var productosBl = new ProductoBibl();
+            var listadeProductos = productosBl.ObtenerProductos();
+            ViewBag.adminWebSiteUrl = ConfigurationManager.AppSettings["adminWebSiteUrl"];
+            return View(listadeProductos);
         }
     }
 }
